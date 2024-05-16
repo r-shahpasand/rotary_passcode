7a3b301e1788d0d55d5ed72c1e103687a9fa9943
Shahpasand <ra.shahpasand@gmail.com>
2024-05-16 21:39:38 +0430

    add dial number widgets on rotary background

--- lib/widgets/rotary_input_widget.dart
+++ lib/widgets/rotary_input_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:rotary_passcode/config/constants.dart';
 import 'package:rotary_passcode/painter/rotary_background_painter.dart';
+import 'package:rotary_passcode/widgets/dial_number_widget.dart';
+import 'dart:math' as math;
 
 class RotaryInputWidget extends StatelessWidget {
   const RotaryInputWidget({Key? key}) : super(key: key);
@@ -10,9 +13,27 @@ class RotaryInputWidget extends StatelessWidget {
       final width = constraints.maxWidth;
       final size = Size(width, width);
 
-      return CustomPaint(
-        size: size,
-        painter: const RotaryDialBackgroundPainter(),
+      final dialNumberDistanceFromCenter = width / 2 -
+          16.0 - /// page padding
+          Constants.rotaryRingPadding * 2 -
+          Constants.dialNumberPadding * 2;
+
+      return Stack(
+        alignment: Alignment.center,
+        children: [
+          CustomPaint(
+            size: size,
+            painter: const RotaryDialBackgroundPainter(),
+          ),
+          for (int i = 0; i < Constants.inputValues.length; i++)
+            Transform.translate(
+              offset: Offset.fromDirection(
+                (i + 1) * -math.pi / 6,
+                dialNumberDistanceFromCenter,
+              ),
+              child: DialNumberWidget(number: Constants.inputValues[i]),
+            )
+        ],
       );
     });
   }
